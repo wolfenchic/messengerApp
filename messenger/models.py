c47e833a562648c7ac85770b3b78977bebc734ef
Nichola Wolfe <wolfe.nichola@gmail.com>
2018-03-08 16:51:23 +0000

    messenger

--- messenger/models.py
+++ messenger/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
-# Create your models here.
 class Message(models.Model):
-    subject = models.Charfield(max_length=50)
-    body = models.Charfield(max_length=50)
-    sender = models.ForeignKey('auth.User', blank=False)
-    recipient = models.ForeignKey('auth.User', blank=False)
-    read = models.BooleanField(default)
+    subject = models.CharField(max_length=50, blank=False)
+    body = models.CharField(max_length=200, blank=False)
+    sender = models.ForeignKey('auth.User', blank=False, related_name="messages_sent")
+    recipient = models.ForeignKey('auth.User', blank=False, related_name="messages_received")
+    read = models.BooleanField(default=False, blank=False)
+    date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.subject
