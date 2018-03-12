8c03436d1708af95e3657bd7faffdcb0892c2721
Nichola Wolfe <wolfe.nichola@gmail.com>
2018-03-12 16:21:56 +0000

    compose and sent message html

--- messenger/urls.py
+++ messenger/urls.py
@@ -4,4 +4,6 @@ from .views import *
 urlpatterns = [
     url(r'^inbox/', inbox, name='inbox'),
     url(r'^mail/(\d+)', message, name='view_message'),
+    url(r'^sent/', sent, name='sent'),
+    url(r'^message/compose', compose_message, name='compose_message'),
 ]
--- messenger/views.py
+++ messenger/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import ComposeMessageForm
 from .models import Message
 
 # Create your views here.
@@ -8,4 +9,25 @@ def inbox(request):
 def message(request, id): 
     message = get_object_or_404(Message, pk=id)
     return render(request, 'messenger/mail.html', {'message': message})
+
+
+def view_message(request, id):
+    message = get_object_or_404(Message, pk=id)
+    message.read=True
+    message.save()
+    return render(request, "messenger/view_message.html", { 'message': message})
+    
+def compose_message(request):
+    if request.method=="POST":
+        form = ComposeMessageForm(request.POST)
+        message = form.save(commit=False)
+        message.sender = request.user
+        message.save()
+        return redirect('inbox')
+    else:
+        form = ComposeMessageForm()
+    
+    return render(request, "messenger/compose_message.html", { 'form': form })
+    
+    
     
